cc70ff6a10f9441fa35f286d19128fd596f2e3f4
KarloKomorski <131690071+KarloKomorski@users.noreply.github.com>
2023-11-17 10:32:20 +0100

    Add project files.

---
+++ Migrations/20231117083242_Add.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCCar.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Car",
+                type: "decimal(8,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(8,2");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Manufacturer",
+                table: "Car",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Manufacturer",
+                table: "Car");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Car",
+                type: "decimal(8,2",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(8,2)");
+        }
+    }
+}
